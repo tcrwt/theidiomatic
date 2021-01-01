e8aaf4e8c6fd828e930a283841071e7b17b999a2
Tim Crowter <tim.crowter@sparx.co.uk>
2021-01-01 17:57:28 +0000

    get the card stack working!

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,16 @@ import { useState } from "preact/hooks";
 import "./app.css";
 import { generateIdiom } from "./shared/idioms";
 
+const numCardsToShow = 5;
+
 export const App = () => {
-  const [idiom, setIdiom] = useState(generateIdiom());
+  const [idiomList, setIdiomList] = useState(
+    Array.from({ length: numCardsToShow }, generateIdiom)
+  );
   const onClick = () => {
-    setIdiom(generateIdiom());
+    const newIdiom = generateIdiom();
+    console.log("newIdiom", newIdiom);
+    setIdiomList((list) => [...idiomList, newIdiom]);
   };
 
   return (
@@ -18,9 +24,37 @@ export const App = () => {
         Generate!
       </button>
       <div className="cardsContainer">
-        <div className="idiomCard">&ldquo;{idiom}&rdquo;</div>
-        <div className="idiomCard">&ldquo;{idiom}&rdquo;</div>
-        <div className="idiomCard">&ldquo;{idiom}&rdquo;</div>
+        {idiomList.map((idiom, i) => {
+          const indexFromEndOfList = idiomList.length - i;
+          const indexInVisibleStack = numCardsToShow - indexFromEndOfList;
+          if (indexInVisibleStack >= 0) {
+            return (
+              <div
+                className="idiomCard inStack"
+                key={i}
+                style={{
+                  //  TODO: this moves everything down a lot, rather than up
+                  //    Really we should do (-20 * (len-i)) so the main card is central
+                  transform: `
+                    translate(0, ${-15 * indexInVisibleStack}px)
+                    scale(${1 - 0.06 * indexInVisibleStack})
+                `,
+                  zIndex: indexFromEndOfList,
+                  opacity: 1 - (indexInVisibleStack - 1) * 0.2,
+                }}
+              >
+                &ldquo;{idiom}&rdquo;
+              </div>
+            );
+          }
+          if (indexInVisibleStack < 0 && indexInVisibleStack > -4) {
+            return (
+              <div className="idiomCard idiomCardSwiped" key={i}>
+                &ldquo;{idiom}&rdquo;
+              </div>
+            );
+          }
+        })}
       </div>
     </div>
   );
