f3a1fa221b38e65f78516d7cabc7100a1f2ec64c
Tim Crowter <tim.crowter@sparx.co.uk>
2021-01-02 16:05:49 +0000

    various design tweaks

--- src/app.tsx
+++ src/app.tsx
@@ -16,42 +16,44 @@ export const App = () => {
       <header className={styles.header}>
         <h1>The Idiomatic</h1>
       </header>
-      <div className={styles.cardsContainer}>
-        {idiomList.map((idiom, i) => {
-          const indexFromEndOfList = idiomList.length - i;
-          const indexInVisibleStack = numCardsToShow - indexFromEndOfList;
-          if (indexInVisibleStack >= 0) {
-            return (
-              <div
-                className={`${styles.idiomCard} ${styles.inStack}`}
-                onClick={onClick}
-                key={i}
-                style={{
-                  transform: `
-                    translate(0, ${-15 * indexInVisibleStack}px)
-                    scale(${1 - 0.06 * indexInVisibleStack})
+      <div className={styles.cardsAndButtonContainer}>
+        <div className={styles.cardsContainer}>
+          {idiomList.map((idiom, i) => {
+            const indexFromEndOfList = idiomList.length - i;
+            const indexInVisibleStack = numCardsToShow - indexFromEndOfList;
+            if (indexInVisibleStack >= 0) {
+              return (
+                <div
+                  className={`${styles.idiomCard} ${styles.inStack}`}
+                  onClick={onClick}
+                  key={i}
+                  style={{
+                    transform: `
+                    translate(0, ${-5 * indexInVisibleStack}%)
+                    scale(${1 - 0.08 * indexInVisibleStack})
                 `,
-                  zIndex: indexFromEndOfList,
-                  opacity: 1 - (indexInVisibleStack - 1) * 0.2,
-                }}
-              >
-                <p className={styles.idiomCardText}>&ldquo;{idiom}&rdquo;</p>
-              </div>
-            );
-          }
-          if (indexInVisibleStack < 0 && indexInVisibleStack > -4) {
-            return (
-              <div className={`${styles.idiomCard} ${styles.idiomCardSwiped}`} key={i}>
-                <p className={styles.idiomCardText}>&ldquo;{idiom}&rdquo;</p>
-              </div>
-            );
-          }
-        })}
-      </div>
-      <div className={styles.generateButtonContainer}>
-        <button className={styles.generateButton} onClick={onClick}>
-          <span className={styles.generateButtonInner}>Generate!</span>
-        </button>
+                    zIndex: indexFromEndOfList,
+                    opacity: 1 - (indexInVisibleStack - 1) * 0.2,
+                  }}
+                >
+                  <p className={styles.idiomCardText}>&ldquo;{idiom}&rdquo;</p>
+                </div>
+              );
+            }
+            if (indexInVisibleStack < 0 && indexInVisibleStack > -4) {
+              return (
+                <div className={`${styles.idiomCard} ${styles.idiomCardSwiped}`} key={i}>
+                  <p className={styles.idiomCardText}>&ldquo;{idiom}&rdquo;</p>
+                </div>
+              );
+            }
+          })}
+        </div>
+        <div className={styles.generateButtonContainer}>
+          <button className={styles.generateButton} onClick={onClick}>
+            <span className={styles.generateButtonInner}>Generate!</span>
+          </button>
+        </div>
       </div>
     </div>
   );
