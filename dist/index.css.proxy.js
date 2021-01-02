5cf9c47a59680762946956576a7456172cf55524
Tim Crowter <tim.crowter@sparx.co.uk>
2021-01-02 21:21:07 +0000

    commit a built version

---
+++ dist/index.css.proxy.js
@@ -0,0 +1,19 @@
+if(typeof document!="undefined"){const e=`html {
+  font-size: 100%
+}
+
+html, body, main {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  overflow: hidden;
+  position: relative;
+}
+`,n=document.createElement("style"),t=document.createTextNode(e);n.type="text/css",n.appendChild(t),document.head.appendChild(n)}
