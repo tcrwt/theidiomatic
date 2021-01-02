dde9c50c67cdf78b43022956d19eb6b2a24fb8ec
Tim Crowter <tim.crowter@sparx.co.uk>
2021-01-02 15:05:53 +0000

    change to sass

--- src/app.tsx
+++ src/app.tsx
@@ -1,36 +1,32 @@
-import { h } from "preact";
-import { useState } from "preact/hooks";
-import "./app.css";
-import { generateIdiom } from "./shared/idioms";
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+import styles from './app.module.css';
+import { generateIdiom } from './shared/idioms';
 
 const numCardsToShow = 5;
 
 export const App = () => {
-  const [idiomList, setIdiomList] = useState(
-    Array.from({ length: numCardsToShow }, generateIdiom)
-  );
+  const [idiomList, setIdiomList] = useState(Array.from({ length: numCardsToShow }, generateIdiom));
   const onClick = () => {
     setIdiomList((list) => [...idiomList, generateIdiom()]);
   };
 
   return (
-    <div className="app">
-      <header className="header">
+    <div className={styles.app}>
+      <header className={styles.header}>
         <h1>The Idiomatic</h1>
       </header>
-      <div className="cardsContainer">
+      <div className={styles.cardsContainer}>
         {idiomList.map((idiom, i) => {
           const indexFromEndOfList = idiomList.length - i;
           const indexInVisibleStack = numCardsToShow - indexFromEndOfList;
           if (indexInVisibleStack >= 0) {
             return (
               <div
-                className="idiomCard inStack"
+                className={`${styles.idiomCard} ${styles.inStack}`}
                 onClick={onClick}
                 key={i}
                 style={{
-                  //  TODO: this moves everything down a lot, rather than up
-                  //    Really we should do (-20 * (len-i)) so the main card is central
                   transform: `
                     translate(0, ${-15 * indexInVisibleStack}px)
                     scale(${1 - 0.06 * indexInVisibleStack})
@@ -39,22 +35,22 @@ export const App = () => {
                   opacity: 1 - (indexInVisibleStack - 1) * 0.2,
                 }}
               >
-                <p className="idiomCardText">&ldquo;{idiom}&rdquo;</p>
+                <p className={styles.idiomCardText}>&ldquo;{idiom}&rdquo;</p>
               </div>
             );
           }
           if (indexInVisibleStack < 0 && indexInVisibleStack > -4) {
             return (
-              <div className="idiomCard idiomCardSwiped" key={i}>
-                <p className="idiomCardText">&ldquo;{idiom}&rdquo;</p>
+              <div className={`${styles.idiomCard} ${styles.idiomCardSwiped}`} key={i}>
+                <p className={styles.idiomCardText}>&ldquo;{idiom}&rdquo;</p>
               </div>
             );
           }
         })}
       </div>
-      <div>
-        <button className="generate" onClick={onClick}>
-          Generate!
+      <div className={styles.generateButtonContainer}>
+        <button className={styles.generateButton} onClick={onClick}>
+          <span className={styles.generateButtonInner}>Generate!</span>
         </button>
       </div>
     </div>
