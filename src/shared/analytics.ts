f166d5f35c0cce7f08a47e1d9f5fc139b638c224
Tim Crowter <tim.crowter@sparx.co.uk>
2021-01-04 18:11:28 +0000

    an even sillier way to get the env vars in
    but it works

--- src/shared/analytics.ts
+++ src/shared/analytics.ts
@@ -1,8 +1,8 @@
 export const initialiseGoogleAnalytics = () => {
-  try {
-    const mode = import.meta?.env?.MODE;
-    gtag('config', 'G-GJREYZZ8L1', { debug_mode: mode === 'development' });
-  } catch (error) {
-    console.error(error);
-  }
+  // try {
+  //   const mode = import.meta?.env?.MODE;
+  // gtag('config', 'G-GJREYZZ8L1', { debug_mode: false });
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
