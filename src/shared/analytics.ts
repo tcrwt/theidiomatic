435550250271ac69f0aa11b520169f560803158a
Tim Crowter <tim.crowter@sparx.co.uk>
2021-01-04 16:30:33 +0000

    stupid way to bypass analytics errors

--- src/shared/analytics.ts
+++ src/shared/analytics.ts
@@ -1,4 +1,8 @@
 export const initialiseGoogleAnalytics = () => {
-  const mode = import.meta?.env?.MODE;
-  gtag('config', 'G-GJREYZZ8L1', { debug_mode: mode === 'development' });
+  try {
+    const mode = import.meta?.env?.MODE;
+    gtag('config', 'G-GJREYZZ8L1', { debug_mode: mode === 'development' });
+  } catch (error) {
+    console.error(error);
+  }
 };
