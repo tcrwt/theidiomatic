0e9f754a6500969ac20ef25ed685983b8636ef50
Tim Crowter <tim.crowter@sparx.co.uk>
2021-01-09 13:48:30 +0000

    try another postcss build

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const autoprefixer = require('autoprefixer');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
-  plugins: [autoprefixer()],
+  plugins: [postcssPresetEnv],
 };
